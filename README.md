1b0076b69257a0e9472eb19a4d30cd8c4d0c34e9
Natan Barbosa <natanbarbosa15@gmail.com>
2020-05-14 16:19:23 -0300

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-# My Web Portfolio
-
-This repository was created for my Web Portfolio page.
-
-[https://natannmb15.github.io/](https://natannmb15.github.io/)
-
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app) and [Bootstrap](https://getbootstrap.com/).
---
+++ precache-manifest.5071b2b91c384303988ff6c31371e4ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d048084eb545e9d8faf7d5def2a14e13",
+    "url": "natannmb15.github.io/index.html"
+  },
+  {
+    "revision": "db1f86d7ed3e41908277",
+    "url": "natannmb15.github.io/static/js/2.82a0fc8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "natannmb15.github.io/static/js/2.82a0fc8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc629ba3c744b422e180",
+    "url": "natannmb15.github.io/static/js/main.020b939f.chunk.js"
+  },
+  {
+    "revision": "94d3c88b8f47a3cc200f",
+    "url": "natannmb15.github.io/static/js/runtime-main.d1b81d44.js"
+  }
+]);
